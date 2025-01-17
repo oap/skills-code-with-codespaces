e068135d8ad10136ce546e2386f1da654e45a9cf
NCai <95010445+oap@users.noreply.github.com>
2025-01-17 02:26:10 +0000

    added more .sh files

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ CURRENT_TIME=$(date '+%Y-%m-%d %H:%M:%S')
 FILE_NAME="newfile.txt"
 
 # Create a new file and add the content
-echo "dotfiles/setup.sh testing $CURRENT_TIME" >> "$FILE_NAME"
+echo "install.sh testing $CURRENT_TIME" >> "$FILE_NAME"
 
 # Print a success message
-echo "File '$FILE_NAME' dotfiles/setup.sh created with content: testing $CURRENT_TIME"
+echo "File '$FILE_NAME'/install.sh created with content: testing $CURRENT_TIME"
