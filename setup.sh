f835e1ebc48169f662ccf77ce4a3b8adae57cfe3
NCai <95010445+oap@users.noreply.github.com>
2025-01-17 02:13:14 +0000

    add new setup.sh file

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Get the current time in a readable format
+CURRENT_TIME=$(date '+%Y-%m-%d %H:%M:%S')
+
+# Define the file name
+FILE_NAME="newfile.txt"
+
+# Create a new file and add the content
+echo "testing $CURRENT_TIME" > "$FILE_NAME"
+
+# Print a success message
+echo "File '$FILE_NAME' created with content: testing $CURRENT_TIME"
