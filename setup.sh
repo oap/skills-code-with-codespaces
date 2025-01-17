8e262f0e54481c903dbb1b514bc4d30b581e6e54
NCai <95010445+oap@users.noreply.github.com>
2025-01-17 05:00:57 +0000

    modified setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ CURRENT_TIME=$(date '+%Y-%m-%d %H:%M:%S')
 FILE_PATH="$HOME/newfile.txt"
 
 # Create a new file in the home directory and add the content
-echo "testing $CURRENT_TIME" >> "$FILE_PATH"
+echo "testing setup.sh $CURRENT_TIME" >> "$FILE_PATH"
 
 # Print a success message
 echo "File '$FILE_PATH' created with content: testing $CURRENT_TIME"
